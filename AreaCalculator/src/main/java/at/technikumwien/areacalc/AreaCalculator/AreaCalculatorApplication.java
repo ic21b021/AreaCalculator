9f1c013dc62f70a01535d7d0c0f92c666e766436
ic21b021 <119104467+ic21b021@users.noreply.github.com>
2023-01-26 17:58:46 +0100

    Initial commit

---
+++ AreaCalculator/src/main/java/at/technikumwien/areacalc/AreaCalculator/AreaCalculatorApplication.java
@@ -0,0 +1,13 @@
+package at.technikumwien.areacalc.AreaCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreaCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AreaCalculatorApplication.class, args);
+	}
+
+}
---
+++ AreaCalculator/src/test/java/at/technikumwien/areacalc/AreaCalculator/AreaCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikumwien.areacalc.AreaCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AreaCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
